d2dbc975487cf17b59d85672d0d985bc786b7cac
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2026-04-15 08:49:42 +0200

    Move inline styles to CSS class for Status component

--- src/components/Status.ts
+++ src/components/Status.ts
@@ -44,12 +44,6 @@ export class Status extends Component {
         this.statusElement = containerEl.createDiv('status-message')
         this.statusElement.addClass('micro-status')
 
-        this.statusElement.style.marginTop = '8px'
-        this.statusElement.style.padding = '6px 10px'
-        this.statusElement.style.backgroundColor = 'var(--background-secondary)'
-        this.statusElement.style.borderRadius = '4px'
-        this.statusElement.style.fontSize = '0.9em'
-
         if (initialMessage) {
             this.setMessage(initialMessage)
         } else {
